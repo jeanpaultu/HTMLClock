6369b022ce69b527d5d170a58f074051a32b5f32
Jean Paul Tu <jp@jeanpaultu.com>
2015-01-28 16:03:50 -0800

    lab4v2

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function getTime() {
+   var d = new Date();
+   var hour = d.getHours();
+   var minutes = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+   var seconds = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
+   var time = hour + ":" + minutes + ":" + seconds;
+   document.getElementById("clock").innerHTML = time;
+   setTimeout(getTime, 1000);
+}
+
+function getTemp() {
+   $.getJSON('https://api.forecast.io/forecast/f20cf5043de9552fca19307b45e486f9/35.300399,-120.662362', function(data) {
+      var label = data['daily']['summary'];
+      console.log(label);
+   });
+}
+
+window.onload = function() {
+   getTime();
+   getTemp();
+}
