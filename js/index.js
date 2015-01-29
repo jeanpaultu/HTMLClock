173d9a28e74483877c8fa028b30141dcdd41d89d
Jean Paul Tu <jp@jeanpaultu.com>
2015-01-28 16:49:10 -0800

    lab4v5

--- js/index.js
+++ js/index.js
@@ -9,15 +9,48 @@ function getTime() {
 }
 
 function getTemp() {
-   var weatherURL = 'https://api.forecast.io/forecast/f20cf5043de9552fca19307b45e486f9/35.300399,-120.662362';
+   var weatherURL = 'https://api.forecast.io/forecast/f20cf5043de9552fca19307b45e486f9/';
+   var latLon;
+
+   if (navigator.geolocation) {
+      latLon = navigator.geolocation.getCurrentPosition(getCoordinates);
+   }
+   else {
+      latLon = '35.300399,-120.662362';
+   }
 
    $.ajax ({
-      url: weatherURL,
+      url: weatherURL + '35.300399,-120.662362',
+      dataType: 'jsonp',
       success: function(data) {
-         var label = data['daily']['summary'];
-         console.log(label);
+         var label = data['daily']['data'][0]['summary'];
+         var icon = data['daily']['data'][0]['icon'];
+         var temp = data['daily']['data'][0]['temperatureMax'];
+
+         $("#forecastLabel").html(label);
+         $("#forecastIcon").attr("src", "img/"+icon+".png");
+         $("body").addClass(getTempClass(temp));
       }
    });
+}
+
+function getCoordinates(position) {
+   console.log(position.coords.latitude);
+   console.log(position.coords.longitude);
+   return position.coords.latitude + ',' + position.coords.longitude;
+}
+
+function getTempClass(temp) {
+   if (temp < 60)
+      return 'cold';
+   else if (temp < 70)
+      return 'chilly';
+   else if (temp < 80)
+      return 'nice';
+   else if (temp < 90)
+      return 'warm';
+   else
+      return 'hot';
 }
 
 window.onload = function() {
