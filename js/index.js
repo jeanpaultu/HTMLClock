b9310a29445f3e401faa934842b64a0ff24a2c6e
Jean Paul Tu <jp@jeanpaultu.com>
2015-02-25 16:50:16 -0800

    adding google analytics 6

--- js/index.js
+++ js/index.js
@@ -119,7 +119,7 @@ function insertAlarm(time, alarmName, objectId) {
 
    div.append("<div class='alarm name'>" + alarmName + "</div>");
    div.append("<div class='alarm time'>" + time + "</div>");
-   div.append("<input type='button' class='button deletebtn' onclick='deleteAlarm(this.id); ga('send', 'event', 'Alarm', 'Delete');' value='Delete' id='" + objectId + "'/>")
+   div.append("<input type='button' class='button deletebtn' onclick='deleteAlarm(this.id);' value='Delete' id='" + objectId + "'/>")
 
    $("#alarms").append(div);
 }
@@ -127,6 +127,8 @@ function insertAlarm(time, alarmName, objectId) {
 function deleteAlarm(objectId) {
    var AlarmObject = Parse.Object.extend("Alarm");
    var query = new Parse.Query(AlarmObject);
+
+   _gaq.push(['_trackEvent', 'Alarm,' 'Delete']);
 
    query.get(objectId, {
       success: function(alarmObject) {
@@ -153,7 +155,7 @@ function addAlarm() {
    var time = hours + ":" + mins + " " + ampm;
    var userID = '';
 
-   
+   _gaq.push(['_trackEvent', 'Alarm,' 'Add']);
 
    var AlarmObject = Parse.Object.extend("Alarm");
    var alarmObject = new AlarmObject();
